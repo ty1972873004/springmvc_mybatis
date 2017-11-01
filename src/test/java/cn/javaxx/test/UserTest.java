1fd44909d158d9a9387e1281631a48aa43b0e939
ty1972873004 <1972873004@qq.com>
2017-11-01 09:09:21 +0800

    junit修改

--- src/test/java/cn/javaxx/test/UserTest.java
+++ src/test/java/cn/javaxx/test/UserTest.java
@@ -2,32 +2,26 @@ package cn.javaxx.test;
 
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.javaxxw.model.User;
 import cn.javaxxw.service.UserService;
 
 
-///** 声明用的是Spring的测试类 **/
-//@RunWith(SpringJUnit4ClassRunner.class)
-///** 声明spring主配置文件位置，注意：以当前测试类的位置为基准,有多个配置文件以字符数组声明 **/
-//@ContextConfiguration(locations = { "classpath:applicationContext-jdbc.xml", "classpath:applicationContext-mybatis.xml" })  
+/** 声明用的是Spring的测试类 **/
+@RunWith(SpringJUnit4ClassRunner.class)
+/** 声明spring主配置文件位置，注意：以当前测试类的位置为基准,有多个配置文件以字符数组声明 **/
+@ContextConfiguration(locations = { "classpath:applicationContext.xml", "classpath:applicationContext-mybatis.xml" })  
 
 public class UserTest {
 	
-		
+		@Autowired
 		private UserService userService;
 		
-		@Before
-	    public void before(){                                                                   
-	        @SuppressWarnings("resource")
-	        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:applicationContext.xml"
-	                ,"classpath:applicationContext-mybatis.xml"});
-	        userService = (UserService) context.getBean("userService");
-	    }
 		
 		//@Test
 	    public void addUser(){
